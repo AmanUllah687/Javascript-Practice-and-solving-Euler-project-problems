2036e464f769d12399edcbfc831ebaee524a5226
Aman Ullah <Amanch687@gmail.com>
2026-01-12 23:30:24 +0500

    Js Function Pass by Value

--- Script.js
+++ Script.js
@@ -1,9 +1,6 @@
-function add(...[a, b, c]) {
-  console.log(a);
-  console.log(b);
-  console.log(c)
-  let total = 0;
- 
+function sum(n1, n2) {
+  n1 = n1 + n2;
+  return n1;
 }
-const result = add(5, 10, 20)
-console.log(result);
+var a= 10 , b = 10;
+console.log(a, "+" , b, "=" ,sum(a, b))

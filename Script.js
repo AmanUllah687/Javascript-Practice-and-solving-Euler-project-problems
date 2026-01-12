c3399c99d9049e130d632761bec826df2c177708
Aman Ullah <Amanch687@gmail.com>
2026-01-12 23:41:40 +0500

    Js functions Pass by Value example 1

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
-function sum(n1, n2) {
-  n1 = n1 + n2;
-  return n1;
+function strFun(p1) {
+  p1 = "eleven"
 }
-var a= 10 , b = 10;
-console.log(a, "+" , b, "=" ,sum(a, b))
+var a = "ten"
+strFun(a);
+console.log(a)

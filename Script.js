b8932d90688d5c9ce4d061f88f98e47c5cb463aa
Aman Ullah <Amanch687@gmail.com>
2026-01-22 23:09:58 +0500

    Private Field Declarations

--- Script.js
+++ Script.js
@@ -1,20 +1,19 @@
 class Rectangle {
-    height = 0;
-    width;
+    #height = 0;
+    #width;
     constructor(height, width) {
-        this.height = height;
-        this.width = width;
+        this.#height = height;
+        this.#width = width;
     }
-    get Height() {
-        return this.height;
+    get Hieght() {
+        return this.#height
     }
     setHeight(height) {
-        this.height = height;
+        this.#height = height
     }
-
 }
-var rect = new Rectangle(10, 15);
-console.log(`Height of rectanle is ${rect.height} cm`);
+var rect = new Rectangle (10, 15);
+console.log(`The Hieght of Rectangle is ${rect.height} cm`)
 console.log(rect.height);
 rect.setHeight(20);
-console.log(rect.height);
+console.log(rect.height)

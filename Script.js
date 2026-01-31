3dcd4e76b0f7b815eb9fbd203c4fc7bf9e0ee93a
Aman Ullah <Amanch687@gmail.com>
2026-01-31 17:44:47 +0500

    if-else Representation

--- Script.js
+++ Script.js
@@ -1,44 +1,7 @@
-class Employee {
-    constructor(name, dept = "general") {
-        this.name = name;
-        this.dept = dept;
-    }
-
-    get Dept() {
-        return this.dept;
-    }
-
-    setDept(dept) {
-        this.dept = dept;
-    }
+let shoppingDone = false;
+let childAllowance;
+if(shoppingDone === true) {
+    childAllowance = 10;
+} else {
+    childAllowance = 5
 }
-
-class Manager extends Employee {
-    constructor(name, dept, reports = []) {
-        super(name, dept);
-        this.reports = reports;
-    }
-}
-
-class WorkerBee extends Employee {
-    constructor(name, dept, projects = []) {
-        super(name, dept);
-        this.projects = projects;
-    }
-}
-
-class SalesPerson extends WorkerBee {
-    constructor(name, dept, projects, quota = 100) {
-        super(name, dept, projects);
-        this.quota = quota;
-    }
-}
-var rect = new SalesPerson("","sales", [], [""], 100);
-class Engineer extends WorkerBee {
-    constructor(name, dept, projects, quota, machine = "") {
-        super(name, dept, projects, quota);
-        this.machine = machine;
-    }
-}
-var Aman = new SalesPerson("", "Engineering", [], [""], 100);
-console.log(Aman.dept);

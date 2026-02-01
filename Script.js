c07a3452019865f0a4376c7b50e7ba52b3f84fd7
Aman Ullah <Amanch687@gmail.com>
2026-02-01 18:45:14 +0500

    Do While loppp

--- Script.js
+++ Script.js
@@ -1,16 +1,5 @@
-function countSelected(selectedObject) {
-  let numberSelected = 0;
-  for(let i = 0; i < selectedObject.options.length; i++) {
-    if(selectedObject.options[i].selected) {
-      numberSelected ++;
-    }
-  }
-  return numberSelected;
-}
-  
-
-const btn = document.getElementById("btn");
-btn.addEventListener("click", () => {
-  const musicTypes = document.selectForm.musicTypes;
-  console.log(`You have Selected ${countSelected(musicTypes)} option(s).`);
-});
+let i = 0;
+do {
+  i += 1;
+  console.log(i);
+} while (i < 5);

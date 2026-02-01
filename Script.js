d6327c053efa36a9c5075d81f06e2e17f26c00aa
Aman Ullah <Amanch687@gmail.com>
2026-02-01 17:47:37 +0500

    for loop

--- Script.js
+++ Script.js
@@ -1,29 +1,16 @@
-const select = document.querySelector("select");
-const html = document.querySelector("html");
-
-select.addEventListener("change", () => {
-  const choice = select.value;
-
-  switch (choice) {
-  case "black":
-      update("black", "white");
-      break;
-    case "white":
-      update("white", "black");
-      break;
-    case "purple":
-      update("purple", "white");
-      break;
-    case "yellow":
-      update("yellow", "purple");
-      break;
-    case "psychedelic":
-      update("lime", "purple");
-      break;
+function countSelected(selectedObject) {
+  let numberSelected = 0;
+  for(let i = 0; i < selectedObject.options.length; i++) {
+    if(selectedObject.options[i].selected) {
+      numberSelected ++;
+    }
+  }
+  return numberSelected;
 }
+  
+
+const btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  const musicTypes = document.selectForm.musicTypes;
+  console.log(`You have Selected ${countSelected(musicTypes)} option(s).`);
 });
-
-function update(bgColor, textColor) {
-  html.style.backgroundColor = bgColor;
-  html.style.color = textColor;
-}

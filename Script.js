72630e1f2d59b6f3f2e64d6987c6a439b3ced0db
Aman Ullah <Amanch687@gmail.com>
2026-02-01 15:14:26 +0500

    Ternary operator Example

--- Script.js
+++ Script.js
@@ -1,6 +1,10 @@
-let isBirthday = true;
-const greeting = isBirthday
-  ? "Happy birthday Mrs. Smith — we hope you have a great day!"
-  : "Good morning Mrs. Smith.";
-
-  console.log(greeting);
+const select = document.querySelector("select");
+const html = document.querySelector("html");
+document.body.style.padding = "10px"
+ function update(bgColor, textColor) {
+  html.style.backgroundColor = bgColor;
+  html.style.color = textColor;
+ }
+ select.addEventListener("change", () =>
+   select.value === "black" ? update("black", "white") : update("white", "black"),
+);

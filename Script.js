bbac7c91661495ff151040eaafcc00233fb9c51e
Aman Ullah <Amanch687@gmail.com>
2026-02-01 22:26:38 +0500

    Example Showing the Diffrence for of loop and for in loop

--- Script.js
+++ Script.js
@@ -1,10 +1,11 @@
-let i = 0;
-let n = 0;
-while(i<5) {
-  i++;
-  if(i === 3) {
-    continue;
-  } 
-  n += i;
-  console.log(n); 
+const arr = [3,5,7];
+arr.foo = "Hello";
+for (const i in arr) {
+  console.log(i);
 }
+// 0,1,2,foo
+
+for(const i of arr) {
+  console.log(i);
+}
+// 3, 5 , 7

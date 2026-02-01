df154cba08a190f9bb8a03df66d876026175e02a
Aman Ullah <Amanch687@gmail.com>
2026-02-01 22:06:31 +0500

    continue statement example

--- Script.js
+++ Script.js
@@ -1,17 +1,10 @@
-let x = 0;
-let z = 0;
-
-labelCancelLoops: while(true) {
-  console.log("Outer Loops:", x);
-  x += 1;
-  z = 1;
-  while(true) {
-    console.log("Inner Loops:", z);
-    z += 1;
-    if( z === 10 && x === 10) {
-      break labelCancelLoops;
-    } else if ( z === 10 ) {
-      break;
-    }
-  }
-} 
+let i = 0;
+let n = 0;
+while(i<5) {
+  i++;
+  if(i === 3) {
+    continue;
+  } 
+  n += i;
+  console.log(n); 
+}

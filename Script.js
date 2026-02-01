22d76ac45cfcd28131e38045257ffe72abf425c6
Aman Ullah <Amanch687@gmail.com>
2026-02-01 20:39:08 +0500

    Breaking to a label

--- Script.js
+++ Script.js
@@ -1,5 +1,17 @@
-let i = 0;
-do {
-  i += 1;
-  console.log(i);
-} while (i < 5);
+let x = 0;
+let z = 0;
+
+labelCancelLoops: while(true) {
+  console.log("Outer Loops:", x);
+  x += 1;
+  z = 1;
+  while(true) {
+    console.log("Inner Loops:", z);
+    z += 1;
+    if( z === 10 && x === 10) {
+      break labelCancelLoops;
+    } else if ( z === 10 ) {
+      break;
+    }
+  }
+} 

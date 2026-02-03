e23423929a1623687372888b4eb4927dba83532e
Aman Ullah <Amanch687@gmail.com>
2026-02-04 00:08:46 +0500

    use of this key word

--- Script.js
+++ Script.js
@@ -1,7 +1,19 @@
-const x = [0,1,2,3,4,5,6,7,8,9];
-const a = [x,x,x,x,x];
-
-for (let i = 0, j = 9; i<=j; i++, j--) {
-
-    console.log(`a[${i}][${j}] = ${a[i][j]}`)
+function getFullName() {
+  return `${this.firstName} ${this.lastName}`;
 }
+const person1 = {
+  firstName: "Chris",
+  lastName: "Martin",
+};
+
+const person2 = {
+  firstName: "Chester",
+  lastName: "Bennington",
+};
+
+// Attach the same function
+person1.getFullName = getFullName;
+person2.getFullName = getFullName;
+
+console.log(person1.getFullName()); // "Chris Martin"
+console.log(person2.getFullName()); // "Chester Bennington"

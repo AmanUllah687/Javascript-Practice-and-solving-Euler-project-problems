387261bec83959d5784dd954647cdb7f9d4dd14b
Aman Ullah <Amanch687@gmail.com>
2026-02-06 02:22:49 +0500

    JavaScript Functions - Function Scopes

--- Script.js
+++ Script.js
@@ -1,9 +1,22 @@
-function foo(i) {
-    if(i<0) {
-        return;
-    }
-    console.log(`begin: ${i}`);
-    foo(i-1);
-    console.log(`end: ${i}`);
+// The followings are defined in global Scope 
+const num1 = 20;
+const num2 = 3;
+const name = "Chamakh";
+
+function multiply() {
+    return num1 * num2;
 }
-foo(3);
+console.log(multiply()); //60
+
+// A nested function Example
+function getScore () {
+    const num1 = 2;
+    const num2 = 3;
+
+    function add() {
+        return `${name} Scored ${num1 + num2}`;
+    }
+    return add();
+}
+
+console.log(getScore());

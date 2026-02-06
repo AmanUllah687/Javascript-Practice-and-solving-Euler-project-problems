91ed8b95133a8ab0396779c3571e360aad5c9306
Aman Ullah <Amanch687@gmail.com>
2026-02-06 23:48:31 +0500

    JavaScript functions- Clossure

--- Script.js
+++ Script.js
@@ -1,22 +1,10 @@
-// The followings are defined in global Scope 
-const num1 = 20;
-const num2 = 3;
-const name = "Chamakh";
-
-function multiply() {
-    return num1 * num2;
-}
-console.log(multiply()); //60
-
-// A nested function Example
-function getScore () {
-    const num1 = 2;
-    const num2 = 3;
-
-    function add() {
-        return `${name} Scored ${num1 + num2}`;
-    }
-    return add();
-}
-
-console.log(getScore());
+// The outer function defines a variable Called "name"
+const pet = function(name) {
+    const getName = function () {
+        // the inner function has the access to "name" vairiable of outer function
+        return name;
+    };
+    return getName; // Return the inner function exposing it to outer scopes 
+}; 
+const myPet = pet("Vivie");
+console.log(myPet());

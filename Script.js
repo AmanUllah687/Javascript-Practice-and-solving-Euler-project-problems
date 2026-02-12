2e09df7afe9e3eceb44b189ce7ee75bf6776088c
Aman Ullah <Amanch687@gmail.com>
2026-02-12 07:32:05 +0500

    Check if array conatins a Certain item

--- Script.js
+++ Script.js
@@ -1,3 +1,5 @@
 const fruits = ["Apple", "Banana"];
-const stringFruit = fruits.join(",");
-console.log(stringFruit);
+console.log(fruits.includes("Apple"));
+console.log(fruits.includes("Cherry"));
+console.log(fruits.indexOf("Banana") !== -1);
+console.log(fruits.indexOf("Cherry") !== -1);

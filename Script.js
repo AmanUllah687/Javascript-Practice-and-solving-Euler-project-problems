754c74e0dcc0fe7122eb9b8f38024d9d58b192f1
Aman Ullah <Amanch687@gmail.com>
2026-02-12 07:51:14 +0500

    Removing the First Element in the Array

--- Script.js
+++ Script.js
@@ -1,7 +1,4 @@
 const fruits = ["Apple", "Banana", "Strawberry", "Mango", "Cherry"];
-const start = 2;
-const removedItems = fruits.splice(start);
+const removedItem = fruits.shift();
 console.log(fruits);
-// ["Apple", "Banana"]
-console.log(removedItems);
-// ["Strawberry", "Mango", "Cherry"]
+console.log(removedItem);

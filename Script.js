d806b68681befd9a645f363fa4387a14c1e4fa44
Aman Ullah <Amanch687@gmail.com>
2026-02-12 07:56:25 +0500

    Removing the Multiple items in The Array

--- Script.js
+++ Script.js
@@ -1,4 +1,6 @@
-const fruits = ["Apple", "Banana", "Strawberry", "Mango", "Cherry"];
-const removedItem = fruits.shift();
+const fruits = ["Apple", "Strawberry", "Cherry", "Banana", "Mango"];
+const start = 0;
+const deletecount = 3;
+const removedItems = fruits.splice(start, deletecount);
 console.log(fruits);
-console.log(removedItem);
+console.log(removedItems);

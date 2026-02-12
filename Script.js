8793505cc0b392a81098b8efc6e3b3a65dda3b95
Aman Ullah <Amanch687@gmail.com>
2026-02-12 23:46:35 +0500

    Iterating over an array

--- Script.js
+++ Script.js
@@ -1,6 +1,4 @@
 const fruits = ["Apple", "Banana", "Strawberry"];
-const start = -2;
-const deleteCount = 2;
-const removedItems = fruits.splice(start, deleteCount, "Mango", "Cherry");
-console.log(fruits);
-console.log(removedItems);
+for(const fruit of fruits) {
+    console.log(fruit);
+}

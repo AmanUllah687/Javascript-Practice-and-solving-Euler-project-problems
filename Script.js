f071a4f6f37bd6517d01e1b23105c84bc603115e
Aman Ullah <Amanch687@gmail.com>
2026-02-12 23:32:18 +0500

    Removing single item by index

--- Script.js
+++ Script.js
@@ -1,4 +1,6 @@
-const fruits = ["Banana", "Apple"]
-const newLength = fruits.unshift("Mango");
+const fruits = ["Strawberry", "Banana", "Mango"];
+const start = fruits.indexOf("Banana");
+const deleteCount = 1;
+const removedItem = fruits.splice(start, deleteCount);
 console.log(fruits);
-console.log(newLength);
+console.log(removedItem);

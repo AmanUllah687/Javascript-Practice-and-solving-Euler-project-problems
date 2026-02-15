abea7c1b4c6673764d0326c75463456e8e0b2189
Aman Ullah <Amanch687@gmail.com>
2026-02-15 17:34:47 +0500

    JavaScript - objects

--- Script.js
+++ Script.js
@@ -1,50 +1,5 @@
-function testSideEffect(effect) {
-  const arr = ["e1", "e2", "e3", "e4"];
-  arr.forEach((elem, index, arr) => {
-    console.log(`array: [${arr.join(", ")}], index: ${index}, elem: ${elem}`);
-    effect(arr, index);
-  });
-  console.log(`Final array: [${arr.join(", ")}]`);
+let x;
+if(true) {
+ x =  {greeting: "Hi There"};
+ console.log(x);
 }
-testSideEffect((arr, index) => {
-  if (index + 1 < arr.length) arr[index + 1] += "*";
-});
-
-testSideEffect((arr, index) => {
-  if (index > 0) arr[index - 1] += "*";
-}); 
-testSideEffect((arr, index) => {
-  if (index === 1) arr.splice(2, 0, "new");
-});
-
-testSideEffect((arr) => arr.push("new"));
-testSideEffect((arr, index) => arr.splice(index, 0, "new"));
-
-testSideEffect((arr, index) => {
-  if (index === 1) arr.splice(2, 1);
-});
-const arr2 = ["e1", "e2", "e3", "e4"];
-arr2.find((elem, index, arr) => {
-  console.log(`array: [${arr.join(", ")}], index: ${index}, elem: ${elem}`);
-  if (index === 1) arr.splice(2, 1);
-  return false;
-});
-testSideEffect((arr, index) => arr.splice(index, 1));
-// array: [e1, e2, e3, e4], index: 0, elem: e1
-// Does not visit e2 because e2 now has index 0, which has already been visited
-// array: [e2, e3, e4], index: 1, elem: e3
-// Does not visit e4 because e4 now has index 1, which has already been visited
-// Final array: [e2, e4]
-// Index 2 is out-of-bounds, so it's not visited
-
-// Compare this with find(), which treats nonexistent indexes as undefined:
-const arr3 = ["e1", "e2", "e3", "e4"];
-arr3.find((elem, index, arr) => {
-  console.log(`array: [${arr.join(", ")}], index: ${index}, elem: ${elem}`);
-  arr.splice(index, 1);
-  return false;
-});
-// array: [e1, e2, e3, e4], index: 0, elem: e1
-// array: [e2, e3, e4], index: 1, elem: e3
-// array: [e2, e4], index: 2, elem: undefined
-// array: [e2, e4], index: 3, elem: undefined

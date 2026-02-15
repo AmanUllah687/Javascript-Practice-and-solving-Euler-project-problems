f4e8ad220858b937683d4e41be4b7e7197461065
Aman Ullah <Amanch687@gmail.com>
2026-02-15 17:42:19 +0500

    Using a constructor function to create objects

--- Script.js
+++ Script.js
@@ -1,5 +1,11 @@
-let x;
-if(true) {
- x =  {greeting: "Hi There"};
- console.log(x);
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
 }
+const myCar = new Car("Eagle", "Talon TSi", 1993);
+const randCar = new Car("Nissan", "300ZX", 1992);
+const kenCar = new Car("Mazda", "Miata", 1990);
+console.log(myCar);
+console.log(randCar);
+console.log(kenCar);

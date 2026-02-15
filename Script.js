2e466aec2905ed0365617c84e0d066b8ac62f0c4
Aman Ullah <Amanch687@gmail.com>
2026-02-15 17:52:40 +0500

    Using a constructor Function

--- Script.js
+++ Script.js
@@ -1,11 +1,14 @@
-function Car(make, model, year) {
+const rand = { name: "Rand", age: 30 };
+const ken = { name: "Ken", age: 28 };
+function Car(make, model, year, owner) {
   this.make = make;
   this.model = model;
   this.year = year;
+  this.owner = owner;
 }
-const myCar = new Car("Eagle", "Talon TSi", 1993);
-const randCar = new Car("Nissan", "300ZX", 1992);
-const kenCar = new Car("Mazda", "Miata", 1990);
-console.log(myCar);
-console.log(randCar);
-console.log(kenCar);
+const car1 = new Car("Eagle", "Talon TSi", 1993, rand);
+const car2 = new Car("Nissan", "300ZX", 1992, ken);
+car1.color = "black";
+console.log(car1);
+console.log(car2);
+

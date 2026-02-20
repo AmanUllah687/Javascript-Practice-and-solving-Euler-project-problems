414d3e4458675a917d86c24ebcf5b39f657a3347
Aman Ullah <Amanch687@gmail.com>
2026-02-20 05:26:07 +0500

    Defining getters and Setters in object

--- Script.js
+++ Script.js
@@ -1,3 +1,19 @@
-const myObj = {a:5, b:12};
-delete myObj.a;
-console.log(myObj);
+const myObj = { a: 0};
+Object.defineProperties(myObj,{
+    b: {
+        get() {
+            return this.a + 1;
+        }
+
+    },
+    c: {
+        set(x) {
+            this.a = x / 2;
+            
+        }
+    },
+
+})
+
+myObj.c = 10; // Runs the setter, which assigns 10 / 2 (5) to the 'a' property
+console.log(myObj.b); // Runs the getter, which yields a + 1 or 6

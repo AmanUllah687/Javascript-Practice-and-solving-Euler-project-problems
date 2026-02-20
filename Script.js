eb7d8c29e4e1e23c4f33d9e03743028b9348a9c5
Aman Ullah <Amanch687@gmail.com>
2026-02-20 05:48:39 +0500

    Truthy and false values in Js

--- Script.js
+++ Script.js
@@ -1,19 +1,3 @@
-const myObj = { a: 0};
-Object.defineProperties(myObj,{
-    b: {
-        get() {
-            return this.a + 1;
-        }
-
-    },
-    c: {
-        set(x) {
-            this.a = x / 2;
-            
-        }
-    },
-
-})
-
-myObj.c = 10; // Runs the setter, which assigns 10 / 2 (5) to the 'a' property
-console.log(myObj.b); // Runs the getter, which yields a + 1 or 6
+if("") {
+    console.log("I am Truthy Value");
+}

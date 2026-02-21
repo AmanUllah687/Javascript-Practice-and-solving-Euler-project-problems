d17e856b6b85313fb88f938e4749092f8dca72e3
Aman Ullah <Amanch687@gmail.com>
2026-02-21 05:53:36 +0500

    Unconditinal Catch block

--- Script.js
+++ Script.js
@@ -1,6 +1,7 @@
 try {
-    throw new TypeError("Oops");
-} catch({name, message}) {
-    console.log(name);
-    console.log(message);
+    throw new Error("My Exeception"); // generates an exception
+
+} catch (e) {
+     // statements to handle any exceptions
+    logMyErrors(e); // pass exception object to error handler
 }

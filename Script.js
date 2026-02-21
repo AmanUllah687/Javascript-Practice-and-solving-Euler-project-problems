afd76398ea1cc4ac24a519347ba6a167052d7af9
Aman Ullah <Amanch687@gmail.com>
2026-02-21 06:06:36 +0500

    Conditinal Catch blocks

--- Script.js
+++ Script.js
@@ -1,7 +1,20 @@
 try {
-    throw new Error("My Exeception"); // generates an exception
-
+ myRoutine() // may throw three types of exceptions
 } catch (e) {
-     // statements to handle any exceptions
+    if(e instanceof TypeError) {
+        // statements to handle TypeError exceptions
+        console.error("Type error:", e.message);
+    } else if(e instanceof RangeError) {
+        // statements to handle RangeError exceptions
+        console.error("Range error:", e.message);
+    } else if(e instanceof EvalError) {
+        // statements to handle EvalError exceptions
+        console.error("Eval error:", e.message);
+    }
+    else {
+        // statements to handle any unspecified exceptions
+        console.error("Unexpected error:", e.message);
+      logMyErrors(e);
     logMyErrors(e); // pass exception object to error handler
+    }
 }

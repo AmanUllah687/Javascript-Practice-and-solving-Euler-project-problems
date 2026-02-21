fcd17924a8f619de031944e007dba5fd16b489b4
Aman Ullah <Amanch687@gmail.com>
2026-02-21 18:52:17 +0500

    Delete Cannot be allowed in Strict Mode

--- Script.js
+++ Script.js
@@ -1,20 +1,4 @@
-try {
- myRoutine() // may throw three types of exceptions
-} catch (e) {
-    if(e instanceof TypeError) {
-        // statements to handle TypeError exceptions
-        console.error("Type error:", e.message);
-    } else if(e instanceof RangeError) {
-        // statements to handle RangeError exceptions
-        console.error("Range error:", e.message);
-    } else if(e instanceof EvalError) {
-        // statements to handle EvalError exceptions
-        console.error("Eval error:", e.message);
-    }
-    else {
-        // statements to handle any unspecified exceptions
-        console.error("Unexpected error:", e.message);
-      logMyErrors(e);
-    logMyErrors(e); // pass exception object to error handler
-    }
-}
+"Use Strict"
+var x = 1;
+Object.hasOwn(globalThis, "x");
+delete globalThis.x

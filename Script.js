8b1f9eee252d0231cd15e36a0f9887efa000c3c4
Aman Ullah <Amanch687@gmail.com>
2026-02-22 06:12:51 +0500

    Using Js functions

--- Script.js
+++ Script.js
@@ -1,6 +1,5 @@
-var a = 0;
- b = 0;
- var a = "A";
- var b = a;
-console.log(a);
-console.log(b);
+function calculateSales(unitsA, unitsB, unitsC) {
+    return unitsA * 79 + unitsB * 129 + unitsC * 699;
+}
+console.log(calculateSales(1,1,1));
+

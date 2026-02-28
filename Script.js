96146d1d6f739e2b80103bd918f07a1e91f5bb0c
Aman Ullah <Amanch687@gmail.com>
2026-02-28 14:55:08 +0500

    Partially Applied function with Settimeout()

--- Script.js
+++ Script.js
@@ -1,19 +1,17 @@
-// Top-level 'this' is bound to 'globalThis' in scripts.
-this.x = 9;
-const module = {
-  x: 81,
-  getX() {
-    return this.x;
-  },
-};
+class LateBloomer {
+  constructor () {
+    this.petalCount = Math.floor(Math.random() * 12) + 1;
+  }
+  bloom () {
+    // Declare a bloom after a delay of Second 
+    setTimeout(this.declare.bind(this), 1000);
+  }
+  declare () {
+    console.log(`I am Beautifull Flower with ${this.petalCount} petals!`);
 
-// The 'this' parameter of 'getX' is bound to 'module'.
-console.log(module.getX()); // 81
+  }
+}
 
-const retrieveX = module.getX;
-// The 'this' parameter of 'retrieveX' is bound to 'globalThis' in non-strict mode.
-console.log(retrieveX()); // 9
-
-// Create a new function 'boundGetX' with the 'this' parameter bound to 'module'.
-const boundGetX = retrieveX.bind(module);
-console.log(boundGetX()); // 81
+const flower = new LateBloomer();
+flower.bloom();
+//// After 1 second, calls 'flower.declare()'

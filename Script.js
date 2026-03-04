39b8162185d7c9a02108dc1ce887cfdad73f157b
Aman Ullah <Amanch687@gmail.com>
2026-03-04 06:21:45 +0500

    Loading an image with XHR

--- Script.js
+++ Script.js
@@ -1,43 +1,51 @@
-"use strict";
-
-let promiseCount = 0;
-
-function testPromise() {
-  const thisPromiseCount = ++promiseCount;
-  const log = document.getElementById("log");
-  // begin
-  log.insertAdjacentHTML("beforeend", `${thisPromiseCount}) Started<br>`);
-  // We make a new promise: we promise a numeric count of this promise,
-  // starting from 1 (after waiting 3s)
-  const p1 = new Promise((resolve, reject) => {
-    // The executor function is called with the ability
-    // to resolve or reject the promise
-    log.insertAdjacentHTML(
-      "beforeend",
-      `${thisPromiseCount}) Promise constructor<br>`,
-    );
-    // This is only an example to create asynchronism
-    setTimeout(
-      () => {
-        // We fulfill the promise
-        resolve(thisPromiseCount);
-      },
-      Math.random() * 2000 + 1000,
-    );
+function imgLoad(url) {
+  // Create new promise with the Promise() constructor;
+  // This has as its argument a function with two parameters, resolve and reject
+  return new Promise((resolve, reject) => {
+    // XHR to load an image
+    const request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.responseType = "blob";
+    // When the request loads, check whether it was successful
+    request.onload = () => {
+      if (request.status === 200) {
+        // If successful, resolve the promise by passing back the request response
+        resolve(request.response);
+      } else {
+        // If it fails, reject the promise with an error message
+        reject(
+          Error(
+            `Image didn't load successfully; error code: + ${request.statusText}`,
+          ),
+        );
+      }
+    };
+    // Handle network errors
+    request.onerror = () => reject(new Error("There was a network error."));
+    // Send the request
+    request.send();
   });
-
-  // We define what to do when the promise is resolved with the then() call,
-  // and what to do when the promise is rejected with the catch() call
-  p1.then((val) => {
-    // Log the fulfillment value
-    log.insertAdjacentHTML("beforeend", `${val}) Promise fulfilled<br>`);
-  }).catch((reason) => {
-    // Log the rejection reason
-    console.log(`Handle rejected promise (${reason}) here.`);
-  });
-  // end
-  log.insertAdjacentHTML("beforeend", `${thisPromiseCount}) Promise made<br>`);
 }
 
-const btn = document.getElementById("make-promise");
-btn.addEventListener("click", testPromise);
+// Get a reference to the body element, and create a new image object
+const body = document.querySelector("body");
+const myImage = new Image();
+const imgUrl =
+  "https://mdn.github.io/shared-assets/images/examples/round-balloon.png";
+
+// Call the function with the URL we want to load, then chain the
+// promise then() method with two callbacks
+imgLoad(imgUrl).then(
+  (response) => {
+    // The first runs when the promise resolves, with the request.response
+    // specified within the resolve() method.
+    const imageURL = URL.createObjectURL(response);
+    myImage.src = imageURL;
+    body.appendChild(myImage);
+  },
+  (error) => {
+    // The second runs when the promise
+    // is rejected, and logs the Error specified with the reject() method.
+    console.log(error);
+  },
+);

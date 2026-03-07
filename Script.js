54fbd945b5f6f8fd0508eb435a3a04ca26ca7b10
Aman Ullah <Amanch687@gmail.com>
2026-03-07 06:10:32 +0500

    Comparing Rest Operator  with Object.assign() in Objects

--- Script.js
+++ Script.js
@@ -1,2 +1,18 @@
-const obj = {...true, ..."test", ... 10},
-// { '0': 't', '1': 'e', '2': 's', '3': 't' }
+const objectAssign = Object.assign({
+    set foo(val) {
+        console.log(val);
+    },
+
+},
+{foo: 1},
+)
+
+// Logs "1"; objectAssign.foo is still the original setter
+
+const spread = {
+    set foo(val) {
+        console.log(val);
+    },
+    ...{foo: 1},
+};
+// Nothing is logged; spread.foo is 1

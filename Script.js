1e1357029fa67723f4dec6e9053ffbb4829cb450
Aman Ullah <Amanch687@gmail.com>
2026-03-09 23:23:24 +0500

    Using the third argument of callbackFn in Array.prototype.find()

--- Script.js
+++ Script.js
@@ -1,7 +1,11 @@
-const myMap = new Map();
-myMap.set("JS", {name: "josh", desc: "I Maintain Things."});
-
-const nameBar = myMap.get("CSS")?.name;
-
-
-console.log(nameBar);
+const numbers = [3, -1, 1, 4, 1, 5, 9, 2, 6];
+const firstThrough = numbers
+.filter((num) => num > 0)
+.find((num, idx, arr) => {
+    // without the arr argument, there is non way to easily acces the
+    // intermedite arry without saving it to a variable 
+    if(idx > 0 && num >= arr[idx - 1]) return false;
+    if(idx < arr.length -1 && num >= arr[idx +1]) return false
+    return true;
+})
+console.log(firstThrough);

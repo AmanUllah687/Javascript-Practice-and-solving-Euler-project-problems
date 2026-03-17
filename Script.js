fad68997e5832aecaa4ff2c452cacf1d5b63f2d7
Aman Ullah <Amanch687@gmail.com>
2026-03-17 20:49:09 +0500

    mouseover Event Example

--- Script.js
+++ Script.js
@@ -1,25 +1,6 @@
-const test = document.getElementById("test");
-
-// This handler will be executed only once when the cursor
-// moves over the unordered list
-test.addEventListener("mouseenter", (event) => {
-  // highlight the mouseenter target
-  event.target.style.color = "purple";
-
-  // reset the color after a short delay
-  setTimeout(() => {
-    event.target.style.color = "";
-  }, 500);
-});
-
-// This handler will be executed every time the cursor
-// is moved over a different list item
-test.addEventListener("mouseover", (event) => {
-  // highlight the mouseover target
-  event.target.style.color = "orange";
-
-  // reset the color after a short delay
-  setTimeout(() => {
-    event.target.style.color = "";
-  }, 500);
-});
+let squareBox = document.querySelector("#box");
+ 
+squareBox.addEventListener("mouseover", () => {
+  squareBox.style.backgroundColor = "green"
+  console.log("i am a Square");
+})

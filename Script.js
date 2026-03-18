fa53e81c0c3b0544648d3a65fe2bc8d3301d236f
Aman Ullah <Amanch687@gmail.com>
2026-03-18 20:50:10 +0500

    Eular Project Problem 4 (Largest Palindrome Product)

--- Script.js
+++ Script.js
@@ -1,22 +1,14 @@
-let PrimeFactors = [];
-for(let i =2; i<600851475143; i++) {
-  if(600851475143 % i === 0) {
-     PrimeFactors.push(i);
-    console.log(i);
- }
-}
-
-// Alternate Soloution for fast proceesing 
-
-let n = 600851475143;
-let factor = 2;
-
-while (factor * factor <= n) {
-  if (n % factor === 0) {
-    n = n / factor;
-  } else {
-    factor++;
+let largest = 0;
+let j;
+for(i=999; i>99; i--) {
+  for(j=999; j>99; j--) {
+    const product = i * j;
+    const str = product.toString();
+    const isPalindrome = str === str.split("").reverse().join("");
+    if(isPalindrome && product > largest) {
+      largest = product;
+    }
   }
 }
 
-console.log(n);
+console.log(largest);

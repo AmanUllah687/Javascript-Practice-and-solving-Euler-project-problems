3367be209aaa0f945b97051676ab7f225dab8345
Aman Ullah <Amanch687@gmail.com>
2026-03-18 06:08:09 +0500

    Finding the 600851475143

--- Script.js
+++ Script.js
@@ -1,6 +1,22 @@
-let squareBox = document.querySelector("#box");
- 
-squareBox.addEventListener("mouseover", () => {
-  squareBox.style.backgroundColor = "green"
-  console.log("i am a Square");
-})
+let PrimeFactors = [];
+for(let i =2; i<600851475143; i++) {
+  if(600851475143 % i === 0) {
+     PrimeFactors.push(i);
+    console.log(i);
+ }
+}
+
+// Alternate Soloution for fast proceesing 
+
+let n = 600851475143;
+let factor = 2;
+
+while (factor * factor <= n) {
+  if (n % factor === 0) {
+    n = n / factor;
+  } else {
+    factor++;
+  }
+}
+
+console.log(n);
--- myModule.js
+++
@@ -1,6 +0,0 @@
-// myModule.js
-let x = 5;
-export const getX = () => x;
-export const setX = (val) => {
-  x = val;
-};

0fca80617ec355522d85f72848e40a2533784310
Aman Ullah <Amanch687@gmail.com>
2026-03-19 20:50:14 +0500

    Added a button

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 const URL = "https://official-joke-api.appspot.com/random_joke";
 const jokePara = document.querySelector("#joke")
+const btn = document.querySelector("#btn")
 const getJokes = async () => {
   console.log("getting data .....");
   let response = await fetch(URL);
@@ -8,3 +9,4 @@ const getJokes = async () => {
   jokePara.innerText = `${data.setup} ... ${data.punchline}`;
 
 }
+btn.addEventListener("click", getJokes);

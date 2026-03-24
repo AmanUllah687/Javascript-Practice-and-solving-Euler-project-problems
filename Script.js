deaa9cd3efcaa56dea23b03d8b0875d472b2e2e3
Aman Ullah <Amanch687@gmail.com>
2026-03-24 23:44:05 +0500

    Eular Project Problem 6 (Sum Square Difference)

--- Script.js
+++ Script.js
@@ -1,63 +1,17 @@
-
-const BASE_URL = `https://api.frankfurter.app/latest`;
-
-const dropdowns = document.querySelectorAll(".dropdown select");
-const btn = document.querySelector("form button");
-const fromCurr = document.querySelector(".from select");
-const toCurr = document.querySelector(".to select");
-const msg = document.querySelector('.msg');
-window.addEventListener("load", () => {
-  updateExchageRate();
-})
-for (let select of dropdowns) {
-    for(let currCode in countryList) {
-        let newOption = document.createElement("option");
-        newOption.innerText = currCode;
-        newOption.value = currCode;
-        if(select.name === "from" &&  currCode === "USD") {
-            newOption.selected = "selected"
-        } else if(select.name === "to" &&  currCode === "INR") {
-            newOption.selected = "selected"
-        }
-        select.append(newOption);
-    }
-    select.addEventListener("change", (evt) => {
-        updateFlag(evt.target);
-    })
+let sumOfSquares = 0;
+for(let i=1; i<=100; i++) {
+    sumOfSquares += i*i;
+}
+console.log(sumOfSquares);
+let sum = 0;
+let squareOfSum = 0;
+for(let j=1; j<=100; j++) {
+    sum += j;
+    squareOfSum = sum * sum;
 }
 
-const updateFlag = (element) => {
-    let currCode = element.value;
-     let countryCode = countryList[currCode];
-    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`
-    let img = element.parentElement.querySelector("img");
-    img.src = newSrc;
-   
+console.log(squareOfSum)
 
-}
 
-btn.addEventListener("click", (evt) => {
-    evt.preventDefault();
-    updateExchageRate();
-  
-
-   
-});
-
- const updateExchageRate = async() => {
-      let amount = document.querySelector(".amount input");
-    let amtVal = amount.value;
-    if(amtVal === "" || amtVal < 1) {
-        amtVal = 1;
-        amount.value ="1";
-    }
- const BASE_URL = `https://api.frankfurter.app/latest`;
-const URL = `${BASE_URL}?from=${fromCurr.value.toUpperCase()}&to=${toCurr.value.toUpperCase()}`;
-
-let response = await fetch(URL);
-let data = await response.json();
-// data.rates["PKR"] instead of data["usd"]["pkr"]
-let rate = data.rates[toCurr.value.toUpperCase()];
-let finalAmount = amtVal * rate;
-msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`
- }
+let diff = squareOfSum - sumOfSquares;
+console.log(diff);

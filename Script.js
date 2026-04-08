c3ea08145b37a36ee94c78e5af76aae1f0bced20
Aman Ullah <Amanch687@gmail.com>
2026-04-08 23:59:20 +0500

    Euler Project Problem 17 (Number Letter Counts)

--- Script.js
+++ Script.js
@@ -1,9 +1,31 @@
-function powerDigitSum () {
-  const powerOfTwo = BigInt(2) ** BigInt(1000);
-  const digiString = powerOfTwo.toString();
-  const sum = digiString.split('').reduce((acc, digit) => acc + Number(digit),0);
-  return sum;
+const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven',
+              'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen',
+              'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty',
+              'sixty', 'seventy', 'eighty', 'ninety'];
+function toWords(n) {
+  if( n === 1000) return "onethousand";
+  if(n<20) {
+   return ones[n];
+  }
+  if(n<100) {
+    return tens[Math.floor(n/10)] + ones[n%10];
+  }
+  if(n<1000) {
+    let hundred = ones[Math.floor(n/100)];
+    let remainder = n % 100;
+    if(remainder === 0) {
+      return hundred + 'hundred';
+    } else {
+     return hundred + 'hundredand' + toWords(remainder);
+    }
+
+  }
 }
-
-
-console.log(powerDigitSum());
+let sum = 0;
+for(let i=1; i<=1000; i++) {
+  let word = toWords(i);
+  sum += word.length;
+}
+console.log(sum);

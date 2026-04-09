25068f7cf70040cfa0d6487f9b26f74957df3738
Aman Ullah <Amanch687@gmail.com>
2026-04-09 19:32:38 +0500

    Euler Project Problem 18 (Maximum Path Sum I)

--- Script.js
+++ Script.js
@@ -1,31 +1,28 @@
-const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven',
-              'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen',
-              'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+const triangle = [
+  [75],
+  [95, 64],
+  [17, 47, 82],
+  [18, 35, 87, 10],
+  [20, 4, 82, 47, 65],
+  [19, 1, 23, 75, 3, 34],
+  [88, 2, 77, 73, 7, 63, 67],
+  [99, 65, 4, 28, 6, 16, 70, 92],
+  [41, 41, 26, 56, 83, 40, 80, 70, 33],
+  [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+  [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+  [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+  [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+  [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+  [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]
+];
 
-const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty',
-              'sixty', 'seventy', 'eighty', 'ninety'];
-function toWords(n) {
-  if( n === 1000) return "onethousand";
-  if(n<20) {
-   return ones[n];
-  }
-  if(n<100) {
-    return tens[Math.floor(n/10)] + ones[n%10];
-  }
-  if(n<1000) {
-    let hundred = ones[Math.floor(n/100)];
-    let remainder = n % 100;
-    if(remainder === 0) {
-      return hundred + 'hundred';
-    } else {
-     return hundred + 'hundredand' + toWords(remainder);
-    }
+for(let row = triangle.length -2; row >=0; row--) {
+  for(let col=0; col<triangle[row].length; col++) {
+    triangle[row][col] += Math.max(
+      triangle[row+1][col],
+      triangle[row+1][col+1]
 
+    ) 
   }
 }
-let sum = 0;
-for(let i=1; i<=1000; i++) {
-  let word = toWords(i);
-  sum += word.length;
-}
-console.log(sum);
+console.log(triangle[0][0]);

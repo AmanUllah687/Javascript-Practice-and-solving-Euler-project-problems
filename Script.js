9f4c18dde70a4e8723dae5cbf5ea344e22edfb09
Aman Ullah <Amanch687@gmail.com>
2026-04-12 20:14:57 +0500

    Euler Project Problem 21 ( Amicable Numbers)

--- Script.js
+++ Script.js
@@ -1,16 +1,20 @@
-function factorial(n) {
-  let result = 1n;
-  for(let i=1; i<=n; i++) {
-    result *= BigInt(i);
-
+function d(n) {
+  let sum = 1;
+for(let i = 2; i<=Math.sqrt(n); i++) {
+  if(n % i === 0) {
+    sum += i;
+    if(i !== n/i) {
+    sum += n/i;
   }
-  return result;
+  }
 }
-
-let result = factorial(100);
-let string = result.toString();
+return sum;
+}
 let sum = 0;
-for(let i=0; i<string.length; i++) {
-  sum += Number(string[i]);
+for(a=1; a<=10000; a++) {
+  let b = d(a);
+  if(d(b) === a && a!==b) {
+    sum += a;
+  }
 }
 console.log(sum);

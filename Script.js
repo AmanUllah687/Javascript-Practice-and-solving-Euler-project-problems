dba0d2908d439f8e23f638101502da46e0cceb35
Aman Ullah <Amanch687@gmail.com>
2026-04-17 17:38:57 +0500

    Euler Project Problem 25 ( 1000-digit Fibonacci Number )

--- Script.js
+++ Script.js
@@ -1,27 +1,17 @@
-function lexicographicPermutation(n) {
-    const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let index = n - 1; // Convert to 0-based index
-    let result = '';
+function firstFibWithNDigits(n) {
+    let a = 1n;
+    let b = 1n;
+    let index = 2;
 
-    // Precompute factorials: fact[i] = i!
-    const fact = [1];
-    for (let i = 1; i <= 9; i++) {
-        fact[i] = fact[i - 1] * i;
+    while(true) {
+        let next  = a + b; 
+        index++;
+        if(String(next).length >= n) {
+            return index;
+        }
+        a = b;
+        b = next;
+
     }
-
-    for (let i = 9; i >= 0; i--) {
-        // Which digit in the remaining list?
-        const digitIndex = Math.floor(index / fact[i]);
-        result += digits[digitIndex];
-
-        // Remove used digit
-        digits.splice(digitIndex, 1);
-
-        // Update index for next position
-        index %= fact[i];
-    }
-
-    return result;
 }
-
-console.log(lexicographicPermutation(1000000)); // 2783915460
+console.log(firstFibWithNDigits(1000)); // 4782

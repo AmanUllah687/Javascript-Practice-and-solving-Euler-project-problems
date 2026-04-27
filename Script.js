86354f1a97424511e0716a470dda137daad785ee
Aman Ullah <Amanch687@gmail.com>
2026-04-27 20:32:58 +0500

    Euler project Problem 32 (Pandigital Products)

--- Script.js
+++ Script.js
@@ -1,10 +1,25 @@
-let coins = [1,2,5,10,20,50,100,200];
-let ways = new Array(201).fill(0);
-ways[0] = 1;
+function isPandigital(a, b, product) {
+let combined = a.toString() + b.toString() + product.toString()
+let sorted    = combined.split("").sort().join('');
+return sorted ==='123456789'
+}
 
-for(let coin of coins) {
-   for(let amount= coin; amount<=200; amount++) {
-      ways[amount] += ways[amount - coin]
+let products = new Set();
+for(a=1; a<=9; a++) {
+   for(b=1000; b<=9999; b++) {
+      let product = a * b;
+     if(isPandigital(a, b, product)) {
+       products.add(product)
+     }
    }
 }
-console.log(ways[200]);
+for(a=10; a<=99; a++) {
+   for(b=100; b<=999; b++) {
+      let product = a * b;
+     if(isPandigital(a, b, product)) {
+       products.add(product)
+     }
+   }
+}
+let answer = Array.from(products).reduce((a,b) => a + b, 0);
+console.log(answer);

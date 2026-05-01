79e0732c9776dcaba80e949d975312aa2b7e5297
Aman Ullah <Amanch687@gmail.com>
2026-05-01 18:12:56 +0500

    Euler Project Problem 36 (Double-base Palindromes)

--- Script.js
+++ Script.js
@@ -1,31 +1,13 @@
-function getRotations(n) {
-  let str = n.toString();
-  let rotations = [];
-  for(i=0; i<=str.length; i++) {
-    rotations.push(Number(str));
-    str = str.slice(1) + str[0];
-
+function isPalindrome(str) {
+  let reversed = str.split('').reverse().join('');
+  return str === reversed;
+}
+let sum = 0;
+ for(let n=1; n<=1000000; n++) {
+  let decimalPallindrome = isPalindrome(n.toString(10));
+  let binaryPallindrome = isPalindrome(n.toString(2));
+  if(decimalPallindrome && binaryPallindrome) {
+    sum += n;
   }
-  return rotations;
-}
-function isPrime(n) {
-    if(n<=1) {
-        return false;
-    }
-    for(let i = 2;i <= Math.sqrt(n); i++) {
-        if(n % i === 0) {
-            return false;
-        }
-    }
-    return true;
-}
-let count=0;
-for(let n=2; n<= 1000000; n++) {
-  let rotations = getRotations(n);
-  let allPrime = rotations.every(r => isPrime(r));
-  if(allPrime) {
-    count++;
-  }
-}
-console.log(count);
-  
+ }
+ console.log(sum);

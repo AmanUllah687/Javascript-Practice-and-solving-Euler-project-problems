a8fa57b3d5d70cb390d28fd00f2ce2ff60fe5285
Aman Ullah <Amanch687@gmail.com>
2026-05-02 18:16:30 +0500

    Euler Project Problem 37 (Truncatable Primes)

--- Script.js
+++ Script.js
@@ -1,13 +1,36 @@
-function isPalindrome(str) {
-  let reversed = str.split('').reverse().join('');
-  return str === reversed;
+function getTruncations(n) {
+  let str = n.toString();
+  let Truncations = [];
+  for(let i=0; i<str.length; i++) {
+    Truncations.push(Number(str.slice(i))); 
+  }
+  for(let i=1; i<str.length; i++) {
+    Truncations.push(Number(str.slice(0, -i)))
+  }
+  return Truncations
+}
+function isPrime(n) {
+  if(n<=1) {
+    return false;
+  }
+  for(i=2; i<=Math.sqrt(n); i++) {
+    if(n % i === 0) {
+      return false
+    }
+  }
+  return true;
 }
 let sum = 0;
- for(let n=1; n<=1000000; n++) {
-  let decimalPallindrome = isPalindrome(n.toString(10));
-  let binaryPallindrome = isPalindrome(n.toString(2));
-  if(decimalPallindrome && binaryPallindrome) {
-    sum += n;
-  }
+let count = 0;
+let n = 11;
+while(count<11) {
+ let Truncations = getTruncations(n);
+ let allPrime = Truncations.every(r => isPrime(r));
+ if(allPrime) {
+  sum += n;
+  count++;
+
  }
- console.log(sum);
+ n++;
+}
+console.log(sum);

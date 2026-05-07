4d300be59a3c4689e4c4798dbbe35fbe508bca29
Aman Ullah <Amanch687@gmail.com>
2026-05-07 12:37:52 +0500

    Leetcode Problem 2 (Rotate String)

--- Script.js
+++ Script.js
@@ -1,16 +1,7 @@
-matrix = [[1,2,3],[4,5,6],[7,8,9]]
-var rotate = function (matrix) {
-  n = matrix.length
-   for(let i=0; i<n; i++) {
-    for(let j=i; j<n; j++) {
-      matrix.flat();
-       [matrix[i][j], matrix[j][i]] =
-      [matrix[j][i], matrix[i][j]];
-    }
-   }
-    for (let row of matrix) {
-    row.reverse();
-  }
-  return matrix;
-};
-console.log(rotate(matrix));
+let s = "abcde";
+let goal = "cdeab";
+var rotateString = function (s,goal) {
+if (s.length !== goal.length) return false
+return (s + s).includes(goal);
+}
+console.log(rotateString(s,goal));

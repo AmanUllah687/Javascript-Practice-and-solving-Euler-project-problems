6e94c3f93c6179c237e848fbc17a494c0f6280a8
Aman Ullah <Amanch687@gmail.com>
2026-05-08 23:26:27 +0500

    Solving LeetCode Problem

--- Script.js
+++ Script.js
@@ -1,7 +1,24 @@
-let s = "abcde";
-let goal = "cdeab";
-var rotateString = function (s,goal) {
-if (s.length !== goal.length) return false
-return (s + s).includes(goal);
+let nums =  [1,2,4,6];
+function isPrime(n) {
+    if(n<=1) return false;
+    for(i=2; i<=Math.sqrt(n); i++) {
+      if(n % i === 0) {
+        return false;
+      }
+    }
+    return true;
 }
-console.log(rotateString(s,goal));
+let moves = [];
+var minJumps = function(nums) {
+    let n = nums.length -1;
+   for(i=0; i<=n; i++){
+    for(j=i; j<=n; j++){
+       moves.push(nums[i]);
+        if(isPrime(nums[i]) && nums[j] % nums[i] === 0) {
+            moves.push(nums[j])
+            
+        }
+        
+   }} return moves.length
+}
+console.log(moves.length);
